2728b01b4aa8a356041efb651c9fdaab8ebba93e
Wesley Silva Gomes <118454287+WesDevss@users.noreply.github.com>
2025-07-12 11:25:23 -0300

    Remove artefatos de build e arquivos sensíveis

--- dist/config/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    port: process.env.PORT || 21465,
-    sessionName: process.env.SESSION_NAME || 'NERDWHATS_AMERICA',
-    enableMultiDevice: process.env.ENABLE_MULTI_DEVICE === 'true'
-};
